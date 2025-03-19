2c78f70c4f9504151fcc8042553e73c84eb43386
vicky-axios <vigneshkumarpmcv@gmail.com>
2025-03-19 16:55:24 +0530

    New changes

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -6,8 +6,11 @@ const Dashboard = () => {
     const userName = location.state?.userName || "User";
 
     return (
-        <div className="text-center mt-10">
-            <h1 className="text-4xl font-bold">Welcome, {userName}!</h1>
+        <div className="text-center mt-10 w-[430px] bg-white p-8 rounded-2xl shadow-lg">
+            <div className=''>
+                <h1 className="text-4xl font-bold">Welcome, {userName}!</h1>
+            </div>
+
         </div>
     );
 };
